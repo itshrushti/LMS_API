c845a558aaf6c92985509003f81c5f1878d748f2
Sagar Dev <devsagar6080@outlook.com>
2025-03-11 15:20:26 +0530

    adding api for complete training in Manageenrollcontroller

--- Controllers/ManageEnrollmentController.cs
+++ Controllers/ManageEnrollmentController.cs
@@ -50,12 +50,10 @@ namespace LMS_Project_APIs.Controllers
                 // Directly start training without approval
                 await _context.Database.ExecuteSqlRawAsync("EXEC start_Training @studentid = {0}, @trainingid = {1}", request.StudentId, request.TrainingId);
                 return Ok("Training started successfully.");
+
             }
 
-            // Call stored procedure
-            //await _context.Database.ExecuteSqlRawAsync("EXEC start_Training @studentid = {0}, @trainingid = {1}", request.StudentId, request.TrainingId);
-
-            //return Ok("Training started successfully.");
+    
 
         }
 
@@ -99,6 +97,27 @@ namespace LMS_Project_APIs.Controllers
             await _context.Database.ExecuteSqlRawAsync("EXEC training_Approval @student_id = {0}, @training_id = {1}, @action = {2}", request.StudentId, request.TrainingId, "DENY");
 
             return Ok("Training request denied successfully.");
+        }
+
+        [HttpPost("CompletedTraining")]
+        public async Task<IActionResult> CompletedTraining([FromBody] TrainingStartModel request)
+        {
+            if (request == null || request.StudentId == null || request.TrainingId == null)
+            {
+                return BadRequest("Invalid data provided.");
+            }
+
+            var studentparam = new SqlParameter("@studentid", request.StudentId ?? (object)DBNull.Value);
+            var trainingparam = new SqlParameter("@trainingid", request.TrainingId ?? (object)DBNull.Value);
+
+            var studentIdParam = new SqlParameter("@studentid", studentparam);
+            var trainingIdParam = new SqlParameter("@trainingid", trainingparam);
+
+            // Call stored procedure
+            await _context.Database.ExecuteSqlRawAsync("EXEC complete_Training @studentid = {0}, @trainingid = {1}", request.StudentId, request.TrainingId);
+
+            return Ok("Training completed successfully.");
         }   
+
     }
 }
